fc2dc5bb34c394e5dceaced96eea9379fc0dfa07
acasanova009 <alfonso.casanova.009@gmail.com>
2021-10-23 23:15:11 -0500

    Actividad5bis terminada

---
+++ Practica6/Actividad5_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(int argc, char){
+    //En este ejercicio imprimiré las iniciales de mi nombre.
+
+    //RAGCG.
+    printf("RRRRRRRRRRRRRRRR\n");
+    printf("        R      R\n");
+    printf("        R      R\n");
+    printf("        RRRRRRRR\n");
+    printf("     RR\n");
+    printf("  RR\n");
+    printf("RR\n");
+    printf("AAAAAAAAAAAAAAAA\n");
+    printf("        A      A\n");
+    printf("        A      A\n");
+    printf("AAAAAAAAAAAAAAAA\n");
+    printf("\n");
+    printf("GGGGGGGGGGGGGGGG\n");
+    printf("G              G\n");
+    printf("G       G      G\n");
+    printf("GGGGGGGG       G\n");
+    printf("\n");
+    printf("CCCCCCCCCCCCCCCC\n");
+    printf("C              C\n");
+    printf("C              C\n");
+    printf("C              C\n");
+    printf("\n");
+    printf("GGGGGGGGGGGGGGGG\n");
+    printf("G              G\n");
+    printf("G       G      G\n");
+    printf("GGGGGGGG       G\n");
+    printf("\n");
+
+    return 0;
+};
