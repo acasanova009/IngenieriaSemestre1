2792994b7beb6d0692d0d467825fb3efffa4719a
acasanova009 <alfonso.casanova.009@gmail.com>
2021-10-23 23:43:26 -0500

    Que chingaos quieren en este ejercicio

---
+++ Practica6/Actividad8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char){
+    int a = 9-5-3;
+    printf("El valor de a es %i\n",a);
+
+    int b = 2 / 3 + 3 / 5;
+    printf("El valor de b %i \n",b);
+    
+    
+    
+    return 0;
+}
