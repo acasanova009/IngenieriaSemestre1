bc51fb50b1b6bd198d7f5c4db9f032bac9896a4c
acasanova009 <alfonso.casanova.009@gmail.com>
2021-10-31 17:56:14 -0600

    Falta actividad B ordenar binario.

--- Practica7/ActividadB.c
+++ Practica7/ActividadB.c
@@ -1,31 +1,5 @@
 #include <stdio.h>
 int main() {
-    // Este programa verifica que si 3 enteros diferentes entre sí están en orden creciente.
-    int ent1, ent2, ent3;
-
-    ent1 =0;
-    ent2 =0;
-    ent3 =0;
-    // Primero verificamos si son diferentes, de otra manera pedimos que vuelva a intentar ingresar 3 enteros diferentes.
-
-    while (ent1==ent2 || ent2==ent3 || ent3==ent1)
-    {
-        printf("Ingresar tres enteros diferentes.\n");
-        scanf("%d",&ent1);
-        scanf("%d",&ent2);
-        scanf("%d",&ent3);
-    }
-
-    
-
-    // Ahora, vamos a corroborar que estén en ordren creciente.
-    if (ent3>ent2 && ent2>ent1)
-    {
-        printf("Sí, los numeros ingresados están en ordren creciente.");
-    }
-    else{
-        printf("No estan en orden creciente.");
-    }
-    
+   //  Este es ordenar binario.
     return 0;
 }
