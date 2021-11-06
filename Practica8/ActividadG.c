08b3d1c08ba3e4b171fe096870e5e5c563bb6ef3
acasanova009 <alfonso.casanova.009@gmail.com>
2021-11-05 19:33:06 -0600

    Fin

---
+++ Practica8/ActividadG.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    
+
+    // bool alternar = true;
+
+    printf("Ingresar 2 numeros,e imrptimir sus tablas de multiplicar\n");
+    int x = 0;
+    int y = 0;
+
+    printf("Ingresar el primer numero\n");
+    scanf("%d", &x);
+    printf("Ingresar el segundo numero\n");
+    scanf("%d", &y);
+
+    for (int i = 1; i <=10; i++){
+        printf("\n\nTabla del %d:\n", i);
+        printf("Primer dígito por %d, es %d:\n", i, i*x);
+        printf("Segundo dígito por %d, es %d:\n", i, i*y);
+    }
+
+
+
+   return 0;
+}
+
