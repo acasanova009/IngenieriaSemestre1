d50e7e684fa7bc3f6c8824c1bfec672e6f97444e
acasanova009 <alfonso.casanova.009@gmail.com>
2021-11-15 10:12:34 -0600

    Review

--- Practica9/ActividadA.c
+++ Practica9/ActividadA.c
@@ -16,10 +16,13 @@ int main() {
     for (int i = 0; i <MAX; i++)
     {
         *(A+i)= rand()%100; 
-        // printf("%d\n",*(A+i));
+        // printf("Contador %d actual: %d\n", i, *(A+i));
 
 
     }
+
+    printf("Este es el septimo numero de arreglo: %d\n", *(A+6));
+    
 
     float B[MAX];
 
@@ -31,18 +34,21 @@ int main() {
     } 
     *(B+4) = 500;
     
+
+
+
     const int newMax = 100;
     float C[newMax];
 
     float  suma = 0;
     float *sumPointer = &suma;
-
-    
-
     for (int i=0;i<newMax;i++)
     {
         *(C+i) =  ((float)rand()/(float)rand());
         *sumPointer += *(C+i);
+        
+        // no valdio
+        // sumPointer += el random 
         // printf("%f\n",*sumPointer);
     }
     printf("Suma final: %f\n",*sumPointer);
