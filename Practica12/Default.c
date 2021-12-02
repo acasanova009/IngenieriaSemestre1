18a9bbaca2813fea3c8da23b37ae95b0e15de7dc
acasanova009 <alfonso.casanova.009@gmail.com>
2021-12-02 13:14:18 -0600

    Ya puedo ver el resultado en format texto

--- Practica12/Default.c
+++ Practica12/Default.c
@@ -6,7 +6,11 @@
 
 
 
+int getNumero(char caracter);
+bool charEsSimbolo(char caracter);
 bool charEsNumero(char caracter);
+int evaluarDatos(int izq, char operacion,int der);
+
 int main()
 {
    FILE *archivo;
@@ -18,33 +22,63 @@ int main()
 
    if (archivo != NULL)
    {
-      printf("El archivo se abrió correctamente.");
+      printf("El archivo se abrió correctamenteABA.");
       printf("\nContenido del archivo:\n");
-      int i = 1;
+      int i = 0;
       while (feof(archivo) == 0)
-         {
+         {   
+
+         int enteroIzq =-1;
+         int enteroDer = -1;
+         char operacion = ' ';
+         int numeroFinal = -1;
+
             fgets (caracteres, 50, archivo);
                // printf("%s fin", caracteres);
+
             for (int j = 0; j < 50; j++){
-               char currentChar  = *(caracteres+j);
-               if (currentChar == '\n' || currentChar == '\000'){
+               char currentChar  = *(caracteres+j);               if (currentChar == '\n' || currentChar == '\000'){
                   // Reniciar el valor de la correpondiente operacion;
                   break;
                }
                if (currentChar == ' ')
-                  continue;
+                 continue;
             
-               // while(charEsNumero(currentChar) || charEsNumero(*(caracteres+j+1))){
-                  if(charEsNumero(*(caracteres+j))){
-                     while(charEsNumero(*(caracteres+j)) || charEsNumero(*(caracteres+j+1))){
+                  if(charEsNumero(currentChar)){
+                     // while(charEsNumero(*(caracteres+j)) || charEsNumero(*(caracteres+j+1))){
                         
-                        printf("%c", currentChar); 
-                        j++;
-                     }
-                  }
-                  else {
+                     //    printf("%c", currentChar); 
+                     //    j++;
+                     // }
+                     //Sería mejor hacer un analizador sintáctico. Pero por timepo.
 
-                        printf("%c", currentChar); 
+                        if (j==0)
+                        {
+                           enteroIzq = getNumero(currentChar);
+                        }
+                        if (j==4)
+                        {
+                           
+                           enteroDer = getNumero(currentChar);
+
+                           numeroFinal = evaluarDatos(enteroIzq, operacion, enteroDer);
+                           //  printf("Op:%i \n", numeroFinal); 
+                            char text[5]; 
+                            sprintf(text, "%d", numeroFinal);
+                            printf("Op:%s \n",  text); 
+                        }
+                        
+                        // printf("%c\n", currentChar); 
+                  }
+                  else if (charEsSimbolo(currentChar)){
+
+
+                        if (j==2)
+                        {
+                           operacion = currentChar;
+                        }
+                        // printf("i:%i \n", j); 
+                        // printf("%c\n", currentChar); 
                   }
 
 
@@ -59,26 +93,26 @@ int main()
 
             }
 
-
+               i++;
          }
          fclose(archivo);
    }
 
-   archivo = NULL;
-   char escribir[]= "Escribir cadena en archivo mediante fputs.\n tFacultad de Ingeniería.\n";
-   archivo = fopen("puts.txt", "r+");
+   // archivo = NULL;
+   // char escribir[]= "Escribir cadena en archivo mediante fputs.\n tFacultad de Ingeniería.\n";
+   // archivo = fopen("puts.txt", "r+");
    
-   if (archivo != NULL)
-   {
-         printf("El archivo se abrió correctamente.\n");
-         fputs (escribir, archivo);
-         fclose(archivo);
-   }
-   else
-   {
-      printf("Error al abrir el archivo.\n");
-      printf("El archivo no existe o no se tienen permisos de lectura.\n");
-   }
+   // if (archivo != NULL)
+   // {
+   //       printf("El archivo se abrió correctamente.\n");
+   //       fputs (escribir, archivo);
+   //       fclose(archivo);
+   // }
+   // else
+   // {
+   //    printf("Error al abrir el archivo.\n");
+   //    printf("El archivo no existe o no se tienen permisos de lectura.\n");
+   // }
 
 
 
@@ -96,4 +130,76 @@ bool charEsNumero(char caracter){
          }
       }
       return esNumero;
+}
+int getNumero(char caracter){
+      int numero = 0;
+
+      for(int i=0; i<10; i++ ){
+         
+         if (caracter == i +'0')
+         {
+            numero = i;
+            break;
+         }
+      }
+      return numero;
+}
+bool charEsSimbolo(char caracter){
+      bool esSimbolo = false;
+
+      switch (caracter)
+      {
+      case '+':
+         esSimbolo =true;
+         break;
+      case '-':
+         esSimbolo =true;
+         break;
+      case '%':
+         esSimbolo =true;
+         break;
+      case '/':
+         esSimbolo =true;
+         break;
+      case '*':
+         esSimbolo =true;
+         break;
+      
+      default:
+         esSimbolo =false;
+         break;
+      }
+      return esSimbolo;
+}
+
+int evaluarDatos(int izq, char operacion,int der){
+      int operacionFinal = -1;
+
+      switch (operacion)
+      {
+      case '+':
+         operacionFinal = izq + der;
+         break;
+      case '-':
+         operacionFinal = izq - der;
+         
+         break;
+      case '%':
+         operacionFinal = izq % der;
+         
+         break;
+      case '/':
+         operacionFinal = izq / der;
+         
+         break;
+      case '*':
+         operacionFinal = izq * der;
+         
+         break;
+      
+      default:
+         operacionFinal = -1;
+         break;
+      }
+      return operacionFinal; 
 }
